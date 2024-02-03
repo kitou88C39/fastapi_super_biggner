71de4c3d0a28c5f98299295ea5a864ca32a4cebe
kitou88C39 <kanfutrooper@icloud.com>
2024-02-03 15:34:12 +0900

    from typing

--- type_hints.py
+++ type_hints.py
@@ -8,13 +8,18 @@ def calc_price_including_tax(price, tax):
 if __name__=='__main__':
     print(f'{calc_price_including_tax(price=price, tax=tax)}円')
 
-# type hints
+# type hints 型ヒントは、変数や関数の引数、戻り値の型を宣言するための機能
 
 price: int = 100
 tax: float = 1.1
 
-def calc_price_including_tax(price: int, tax: float):
+def calc_price_including_tax(price: int, tax: float) -> int:
     return int(price*tax)
 
 if __name__=='__main__':
     print(f'{calc_price_including_tax(price=price, tax=tax)}円')
+
+# from typing は、Python のコードで型ヒントを使用するために必要なモジュール
+from typing import List, Dirt
+
+sample_list: List[int] = [1, 2, 3, 4]
