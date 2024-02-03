d24d8bcaae896d06a8b8cfc5b65fb91ed04e2855
kitou88C39 <kanfutrooper@icloud.com>
2024-02-03 09:49:37 +0900

    type hints

--- type_hints.py
+++ type_hints.py
@@ -2,6 +2,7 @@ price = 100
 tax = 1.1
 
 def calc_price_including_tax(price, tax):
+    return int(price*tax)
 
 if __name__=='__main__':
-    print(f'calc_price_including_tax(price=price, tax=tax)')
+    print(f'{calc_price_including_tax(price=price, tax=tax)}円')
