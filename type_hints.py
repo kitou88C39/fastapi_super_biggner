859228755c045bd65700dc7e107ee5ddbe2b420c
kitou88C39 <kanfutrooper@icloud.com>
2024-02-03 11:13:18 +0900

    type hints

--- type_hints.py
+++ type_hints.py
@@ -1,7 +1,19 @@
+# Nomal
 price = 100
 tax = 1.1
 
 def calc_price_including_tax(price, tax):
+    return int(price*tax)
+
+if __name__=='__main__':
+    print(f'{calc_price_including_tax(price=price, tax=tax)}円')
+
+# type hints
+
+price: int = 100
+tax: float = 1.1
+
+def calc_price_including_tax(price: int, tax: float):
     return int(price*tax)
 
 if __name__=='__main__':
