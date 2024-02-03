9d0216d34b03d04392414c787104a9f2d299263b
kitou88C39 <kanfutrooper@icloud.com>
2024-02-03 16:36:33 +0900

    パスパラメータ

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ app = FastAPI()
 @app.get("/")
 async def index():
     return {"message": "Hello World"}
+
+# パスパラメータ
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/contries")
+async def contry():
+    return {"contry_name": contry_name}
